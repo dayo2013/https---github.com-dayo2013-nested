6e38ab6ab6face2cf85eb8775c2a1cbe21926498
dayo2013 <emmanuelagbaje501@gmal.com>
2023-08-21 00:21:57 +0100

    nested

---
+++ doublenested.sol
@@ -0,0 +1,13 @@
+contract doubleMapping{
+    mapping (address => mapping (uint => bool))public nested;
+    function get (address _addr, uint _i)public view returns (bool){
+        return nested[_addr][_i];
+    }
+    function set (address _addr , uint _i, bool _boo)public {
+        nested[_addr][_i] = _boo;
+    }
+     function remove(address _addr, uint _i)public {
+         delete nested[_addr][_i];
+     }
+
+}
